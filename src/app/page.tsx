1382a8ba593e9b3058858f570432e3ed96b03ab2
Asia-parveen <beautyhealthytips@gmail.com>
2024-10-28 13:49:56 -0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import NavBar from './components/navbar/NavBar';
+
 
 import Hero from './components/hero/Hero';
 import ManuSection from './components/manuSection/ManuSection';
 import FoodSection from './components/food/FoodSection';
 import OurManu from './components/ourmanu/OurManu';
-import Footer from './components/footer/Footer';
+
 
 
 
@@ -13,12 +13,12 @@ import Footer from './components/footer/Footer';
 const page = () => {
   return (
     <div>
-      {/* <NavBar/> */}
+     
       <Hero/>
       <ManuSection/>
       <FoodSection/>
       <OurManu/>
-     {/* <Footer/> */}
+    
     
 
     </div>
--- src/app/pagess/page.tsx
+++ src/app/pagess/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import NavBar from '../components/navbar/NavBar'
+
 import FoodSection from '../components/food/FoodSection'
-import Footer from '../components/footer/Footer'
+
 import OurManu from '../components/ourmanu/OurManu'
 
 const page = () => {
